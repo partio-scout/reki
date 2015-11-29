4fa3f00edf7145a541f9168b40a2e6d90a6309b4
Miika Hänninen <miika.hanninen@gmail.com>
2015-11-29 15:37:41 +0200

    Add base ui components

---
+++ src/client/components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Navbar, Grid } from 'react-bootstrap';
+import { Link } from 'react-router';
+
+export default function getApp() {
+  class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to="/">REKI</Link>
+              </Navbar.Brand>
+            </Navbar.Header>
+          </Navbar>
+          <Grid>
+            { this.props.children }
+          </Grid>
+        </div>
+      );
+    }
+  }
+
+  App.propTypes = {
+    children: React.PropTypes.node,
+  };
+
+  return App;
+}
---
+++ src/client/main.jsx
@@ -0,0 +1,22 @@
+// This require tells webpack to compile the stylesheet
+require('./styles.scss');
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute } from 'react-router';
+
+import getApp from './components/App';
+import getHomepage from './components/Homepage';
+
+const app = getApp();
+const homepage = getHomepage();
+
+const routes = (
+  <Router>
+    <Route path="/" component={ app }>
+      <IndexRoute component={ homepage } />
+    </Route>
+  </Router>
+);
+
+render(routes, document.body);
