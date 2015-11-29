ebc561e67beb48a82d29c2f029857ea22dfb7420
Miika Hänninen <miika.hanninen@gmail.com>
2015-11-29 17:50:58 +0200

    Add webpack dev server

--- src/server/boot/04-frontend.js
+++ src/server/boot/04-frontend.js
@@ -3,6 +3,56 @@ const path = require('path');
 
 const publicPath = path.resolve(__dirname, '../../public');
 
+function startDevServer() {
+  const webpack = require('webpack');
+  const WebpackDevServer = require('webpack-dev-server');
+  const webpackConfig = require('./../../../webpack.development.config');
+
+  let bundleStart = null;
+  const compiler = webpack(webpackConfig);
+  compiler.plugin('compile', () => {
+    console.log('Bundling project, please wait...');
+    bundleStart = Date.now();
+  });
+  compiler.plugin('done', () => console.log(`Bundled in ${Date.now() - bundleStart} ms!`));
+
+  const bundler = new WebpackDevServer(compiler, {
+    publicPath: '/build/',
+    inline: true,
+    hot: true,
+    quiet: false,
+    noInfo: true,
+    stats: {
+      colors: true,
+    },
+  });
+
+  bundler.listen(3001, 'localhost');
+}
+
+function redirectBuildPathToDevServer(server) {
+  const httpProxy = require('http-proxy');
+  const proxy = httpProxy.createProxyServer({
+    changeOrigin: true,
+    ws: true,
+  });
+
+  server.all('/build/*', (req, res) => proxy.web(req, res, { target: 'http://127.0.0.1:3001' }));
+  server.all('/sockjs-node/*', (req, res) => proxy.web(req, res, { target: 'http://127.0.0.1:3001' }));
+
+  // Just catch all errors, don't care about how to treat them
+  proxy.on('error', e => console.error(e));
+
+  server.on('upgrade', (req, socket, head) => proxy.ws(req, socket, head));
+}
+
 module.exports = function(server) {
-  server.on('started', () => server.use(loopback.static(publicPath)));
+  server.on('started', () => {
+    if (server.get('isDev')) {
+      startDevServer();
+      redirectBuildPathToDevServer(server);
+    }
+
+    server.use(loopback.static(publicPath));
+  });
 };
