50777aab736ab6fdcb99ebff24df45634282a8fe
Miika Hänninen <miika.hanninen@gmail.com>
2015-12-08 20:00:03 +0200

    Add create-user script for adding users

---
+++ scripts/create-user.js
@@ -0,0 +1,46 @@
+const Promise = require('bluebird');
+
+const path = require('path');
+const serverPath = path.resolve(__dirname, '../src/server');
+const app = require(serverPath + '/server.js');
+const crypto = require('crypto');
+
+const Registryuser = app.models.Registryuser;
+const createUser = Promise.promisify(Registryuser.create, { context: Registryuser });
+
+const opts = require('commander')
+  .usage('<member number> <user email>')
+  .parse(process.argv);
+
+if (opts.args.length < 2) {
+  opts.outputHelp();
+  console.error('Please provide the user\'s member number and email.');
+  process.exit(1);
+}
+
+const memberNumber = opts.args[0];
+const email = opts.args[1];
+const password = crypto.randomBytes(24).toString('hex');
+const user = {
+  memberNumber: memberNumber,
+  email: email,
+  password: password,
+  name: 'n/a',
+  phone: 'n/a',
+};
+
+function printErrorMessage(err) {
+  // Koska template literalit on multiline, alla oleva sisennys on tarkoituksenmukaista
+  console.error(`Could not create user:
+  ${err.stack}`);
+}
+
+createUser(user)
+  .then(createdUserInfo => {
+    console.log(`Created user with id ${createdUserInfo.id}`);
+    process.exit(0);
+  })
+  .catch(err => {
+    printErrorMessage(err);
+    process.exit(1);
+  });
