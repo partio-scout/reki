3c92d5ed35312c4d642574d59cd52678ec741a18
ritvje <jenna.ritvanen@gmail.com>
2016-02-07 21:11:11 +0200

    Add participant's date of birth to view

--- src/client/components/ParticipantDetailsPage.jsx
+++ src/client/components/ParticipantDetailsPage.jsx
@@ -26,15 +26,20 @@ export function getParticipantDetailsPage(participantStore, participantActions) 
     render() {
       let participantName = '';
       let nonScout = '';
+      let dateOfBirth = '';
       if (this.state.participantDetails) {
         participantName = `${this.state.participantDetails.firstName} ${this.state.participantDetails.lastName}`;
         nonScout = this.state.participantDetails.nonScout ? 'EVP' : 'Partiolainen';
+        const [year, month, time] = `${this.state.participantDetails.dateOfBirth}`.split('-');
+        const day = `${time}`.substring(0,2);
+        dateOfBirth = `${day}.${month}.${year}`;
       }
 
       return (
         <div>
-          <h1><b>{ participantName }</b></h1>
-          <p>{ nonScout }</p>
+          <h2><b>{ participantName }</b></h2>
+          <p className="text-muted">{ nonScout }</p>
+          <p> <b> Syntymäaika: </b> { dateOfBirth }</p>
         </div>
       );
     }
