82c5dbc02e45fc6d87c93a68ec7558d9303055e0
ritvje <jenna.ritvanen@gmail.com>
2016-02-09 21:51:05 +0200

    Add first view of participant contact details

--- src/client/components/ParticipantDetailsPage.jsx
+++ src/client/components/ParticipantDetailsPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Row, Col, Panel } from 'react-bootstrap';
 
 export function getParticipantDetailsPage(participantStore, participantActions) {
   class ParticipantDetailsPage extends React.Component {
@@ -27,19 +28,43 @@ export function getParticipantDetailsPage(participantStore, participantActions) 
       let participantName = '';
       let nonScout = '';
       let dateOfBirth = '';
+      let participantPhone = '';
+      let homeCity = '';
+      let email = '';
       if (this.state.participantDetails) {
         participantName = `${this.state.participantDetails.firstName} ${this.state.participantDetails.lastName}`;
         nonScout = this.state.participantDetails.nonScout ? 'EVP' : 'Partiolainen';
         const [year, month, time] = `${this.state.participantDetails.dateOfBirth}`.split('-');
         const day = `${time}`.substring(0,2);
         dateOfBirth = `${day}.${month}.${year}`;
+        participantPhone = `0${this.state.participantDetails.phoneNumber}`;
+        homeCity = `${this.state.participantDetails.homeCity}`;
+        email = `${this.state.participantDetails.email}`;
       }
 
       return (
         <div>
-          <h2><b>{ participantName }</b></h2>
-          <p className="text-muted">{ nonScout }</p>
-          <p> <b> Syntymäaika: </b> { dateOfBirth }</p>
+          <Grid>
+            <Row>
+              <Col>
+                <h2><b>{ participantName }</b></h2>
+                <p className="text-muted">{ nonScout }</p>
+                <p> <b> Syntymäaika: </b> { dateOfBirth }</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={ 3 }>
+                <Panel header="Yhteystiedot">
+                  <p className="text-muted"> Puhelin </p>
+                  <p >{ `${participantPhone}` }</p>
+                  <p className="text-muted"> Kotikaupunki</p>
+                  <p >{ `${homeCity}` }</p>
+                  <p className="text-muted"> Sähköposti</p>
+                  <p >{ `${email}` }</p>
+                </Panel>
+              </Col>
+            </Row>
+          </Grid>
         </div>
       );
     }
