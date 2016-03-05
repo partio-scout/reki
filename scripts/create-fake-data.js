2392dfc37a37b79cc157a4a2f5a791edf04411bf
Masa <matti.sippola@aalto.fi>
2016-03-05 12:01:39 +0200

    Generate random details for a participant
    Finnish was not available so Italy is used for the fun!

--- scripts/create-fake-data.js
+++ scripts/create-fake-data.js
@@ -1,19 +1,23 @@
+import Promise from 'bluebird';
 import app from '../src/server/server.js';
 import faker from 'faker';
 
 const ParticipantModel = app.models.Participant;
+faker.locale = 'it';
 
-const email = faker.internet.email();
+let dob = faker.date.past(30, new Date("Sat Sep 20 2004"));
+dob = dob.getFullYear() + "-" + dob.getMonth() + "-" + dob.getDate();
 
 const participant = {
-  "firstName": "Eeppinen",
-  "lastName": "Trolli",
-  "nonScout": false,
-  "memberNumber": 1234567,
-  "dateOfBirth": "1583-01-01",
-  "phoneNumber": "0501234567",
-  "email": email,
-  "homeCity": "Middle Earth"
+  "firstName": faker.name.firstName(),
+  "lastName": faker.name.lastName(),
+  "nonScout": Math.random() < 0.1,
+  "memberNumber": Math.floor(Math.random()*10000000),
+  "dateOfBirth": dob,
+  "phoneNumber": faker.phone.phoneNumber('050 #######'),
+  "email": faker.internet.email(),
+  "homeCity": faker.address.city()
 }
-
+console.log('Attempting to create fake data.');
 ParticipantModel.create(participant);
+console.log('Succesfully created a fake participant!');
