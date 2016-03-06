8e3b9daee3656d4aaa7158574db205ffb6b8be1b
ritvje <jenna.ritvanen@gmail.com>
2016-03-06 12:37:38 +0200

    Add audit logging for individual participant & registryuser for create, update, find, remove

--- src/common/models/audit-event.js
+++ src/common/models/audit-event.js
@@ -1,2 +1,29 @@
 export default function (AuditEvent) {
+  AuditEvent.createEvent = {};
+
+  AuditEvent.createEvent.Participant = function(userId, instanceId, description) {
+    AuditEvent.create(
+      {
+        'eventType': description,
+        'model': 'Participant',
+        'modelId': instanceId,
+        'timestamp': (new Date()).toISOString(),
+        'registryuserId': userId,
+        'comment': '',
+      },
+      (err,instance) => {if (err) {throw err;} return;});
+  };
+
+  AuditEvent.createEvent.Registryuser = function(userId, instanceId, description) {
+    AuditEvent.create(
+      {
+        'eventType': description,
+        'model': 'Registryuser',
+        'modelId': instanceId,
+        'timestamp': (new Date()).toISOString(),
+        'registryuserId':  userId,
+        'comment': '',
+      },
+      (err,instance) => {if (err) {throw err;} return;});
+  };
 }
--- src/common/models/participant.js
+++ src/common/models/participant.js
@@ -1,2 +1,38 @@
+import app from '../../server/server.js';
+import Promise from 'bluebird';
+import loopback from 'loopback';
+
 export default function (Participant) {
+  Participant.afterRemote('create', (ctx, participantInstance, next) => {
+    const userId = ctx.req.accessToken ? ctx.req.accessToken.userId : 0;
+    app.models.AuditEvent.createEvent.Participant(userId, participantInstance.participantId, 'add');
+    next();
+  });
+
+  Participant.afterRemote('findById', (ctx, participantInstance, next) => {
+    const userId = ctx.req.accessToken ? ctx.req.accessToken.userId : 0;
+    app.models.AuditEvent.createEvent.Participant(userId, participantInstance.participantId, 'find');
+    next();
+  });
+
+  Participant.afterRemote('prototype.updateAttributes', (ctx, participantInstance, next) => {
+    const userId = ctx.req.accessToken ? ctx.req.accessToken.userId : 0;
+    app.models.AuditEvent.createEvent.Participant(userId, participantInstance.participantId, 'update');
+    next();
+  });
+
+  Participant.observe('before delete', (ctx, next) => {
+    const findParticipant = Promise.promisify(app.models.Participant.find, { context: app.models.Participant });
+    if (ctx.instance) {
+      const userId = loopback.getCurrentContext().get('accessToken') ? loopback.getCurrentContext().get('accessToken').userId : 0;
+      app.models.AuditEvent.createEvent.Participant(userId, ctx.instance.participantId, 'delete');
+      next();
+    } else {
+      findParticipant({ where: ctx.where, fields: { participantId: true } })
+        .each(participant => {
+          const userId = loopback.getCurrentContext().get('accessToken') ? loopback.getCurrentContext().get('accessToken').userId : 0;
+          app.models.AuditEvent.createEvent.Participant(userId, participant.participantId, 'delete');
+        }).nodeify(next);
+    }
+  });
 }
