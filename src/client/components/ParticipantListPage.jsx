07681e96c2fb3386a7d58dfa9c9caeeaa366bea6
Miika Hänninen <miika.hanninen@gmail.com>
2016-03-16 16:06:39 +0200

    Separate logic into container components
    Now each smaller piece of the page will hande its own updating, and the
    logic remains localized. Also, the entire page never needs to rerender.

--- src/client/components/ParticipantListPage.jsx
+++ src/client/components/ParticipantListPage.jsx
@@ -8,29 +8,17 @@ export function getParticipantListPage(participantStore, participantActions) {
   const ParticipantCountUpdater = getParticipantCountUpdater(participantActions);
   const SortableHeaderCell = getSortableHeaderCell();
 
-  function extractState(newState) {
-    const {
-      participants,
-      participantsOffset: offset,
-      participantLimit: limit,
-      participantListOrder: order,
-      participantCount: count,
-    } = newState;
-
-    return {
-      participants,
-      offset,
-      limit,
-      order,
-      count,
-    };
-  }
-
-  class ParticipantListPage extends React.Component {
+  class SortableHeaderCellContainer extends React.Component {
     constructor(props) {
       super(props);
 
-      this.state = extractState(participantStore.getState());
+      this.state = this.extractState(participantStore.getState());
+    }
+
+    extractState(nextState) {
+      return {
+        order: nextState.participantListOrder,
+      };
     }
 
     componentDidMount() {
@@ -41,61 +29,145 @@ export function getParticipantListPage(participantStore, participantActions) {
       participantStore.unlisten(this.onParticipantStoreChange.bind(this));
     }
 
-    shouldComponentUpdate(newProps, newState) {
-      const participantsChanged = !_.isEqual(this.state.participants, newState.participants);
-      const countChanged = this.state.count !== newState.count;
-      return participantsChanged || countChanged;
-    }
-
     onParticipantStoreChange(state) {
-      const newState = extractState(state);
+      const newState = this.extractState(state);
       this.setState(newState);
-    }
-
-    handleOffsetSelectionChanged(newOffset) {
-      participantActions.changeParticipantListOffset(newOffset);
     }
 
     handleOrderSelectionChanged(newOrder) {
       participantActions.changeParticipantListOrder(newOrder);
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+      return !_.isEqual(this.props, nextProps) || !_.isEqual(this.state, nextState);
+    }
+
     render() {
       return (
-        <div>
-          <ParticipantListUpdater />
-          <ParticipantCountUpdater />
-
-          <h1>Leiriläiset</h1>
-          <ListOffsetSelector
-            offset={ this.state.offset }
-            count={ this.state.count }
-            chunkSize={ this.state.limit }
-            onOffsetChanged={ this.handleOffsetSelectionChanged }
-          />
-          <Table striped>
-            <thead>
-              <tr>
-                <SortableHeaderCell label="Etunimi" property="firstName" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Sukunimi" property="lastName" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Syntymäpäivä" property="dateOfBirth" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Sukupuoli" property="gender" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Onko partiolainen?" property="nonScout" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Jäsennumero" property="memberNumber" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Kotikaupunki" property="homeCity" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Uimataito" property="swimmingSkill" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Home hospitality" property="interestedInHomeHospitality" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Sähköposti" property="email" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-                <SortableHeaderCell label="Puhelinnumero" property="phoneNumber" order={ this.state.order } orderChanged={ this.handleOrderSelectionChanged } />
-              </tr>
-            </thead>
-            <tbody>
-              { this.state.participants.map(participant => <ParticipantRow key={ participant.participantId } participant={ participant } />) }
-            </tbody>
-          </Table>
-        </div>
+        <SortableHeaderCell
+          { ...this.props }
+          order={ this.state.order }
+          orderChanged={ this.handleOrderSelectionChanged }
+        />
       );
     }
+  }
+
+  class ParticipantRowsContainer extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = this.extractState(participantStore.getState());
+    }
+
+    extractState(nextState) {
+      return {
+        participants: nextState.participants,
+      };
+    }
+
+    componentDidMount() {
+      participantStore.listen(this.onParticipantStoreChange.bind(this));
+    }
+
+    componentWillUnmount() {
+      participantStore.unlisten(this.onParticipantStoreChange.bind(this));
+    }
+
+    onParticipantStoreChange(state) {
+      const newState = this.extractState(state);
+      this.setState(newState);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+      return !_.isEqual(this.state, nextState);
+    }
+
+    render() {
+      return (
+        <tbody>
+          { this.state.participants.map(participant => <ParticipantRow key={ participant.participantId } participant={ participant } />) }
+        </tbody>
+      );
+    }
+  }
+
+  class ListOffsetSelectorContainer extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = this.extractState(participantStore.getState());
+    }
+
+    extractState(nextState) {
+      return {
+        offset: nextState.participantsOffset,
+        limit: nextState.participantLimit,
+        count: nextState.participantCount,
+      };
+    }
+
+    componentDidMount() {
+      participantStore.listen(this.onParticipantStoreChange.bind(this));
+    }
+
+    componentWillUnmount() {
+      participantStore.unlisten(this.onParticipantStoreChange.bind(this));
+    }
+
+    onParticipantStoreChange(state) {
+      const newState = this.extractState(state);
+      this.setState(newState);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+      return !_.isEqual(this.state, nextState);
+    }
+
+    handleOffsetSelectionChanged(newOffset) {
+      participantActions.changeParticipantListOffset(newOffset);
+    }
+
+    render() {
+      return (
+        <ListOffsetSelector
+          offset={ this.state.offset }
+          count={ this.state.count }
+          chunkSize={ this.state.limit }
+          onOffsetChanged={ this.handleOffsetSelectionChanged }
+        />
+      );
+    }
+  }
+
+  function ParticipantListPage() {
+    return (
+      <div>
+        <ParticipantListUpdater />
+        <ParticipantCountUpdater />
+
+        <h1>Leiriläiset</h1>
+        <ListOffsetSelectorContainer />
+        <Table striped>
+          <thead>
+            <tr>
+              <SortableHeaderCellContainer label="Etunimi" property="firstName" />
+              <SortableHeaderCellContainer label="Sukunimi" property="lastName" />
+              <SortableHeaderCellContainer label="Syntymäpäivä" property="dateOfBirth" />
+              <SortableHeaderCellContainer label="Sukupuoli" property="gender" />
+              <SortableHeaderCellContainer label="Onko partiolainen?" property="nonScout" />
+              <SortableHeaderCellContainer label="Jäsennumero" property="memberNumber" />
+              <SortableHeaderCellContainer label="Kotikaupunki" property="homeCity" />
+              <SortableHeaderCellContainer label="Uimataito" property="swimmingSkill" />
+              <SortableHeaderCellContainer label="Home hospitality" property="interestedInHomeHospitality" />
+              <SortableHeaderCellContainer label="Sähköposti" property="email" />
+              <SortableHeaderCellContainer label="Puhelinnumero" property="phoneNumber" />
+            </tr>
+          </thead>
+          <ParticipantRowsContainer />
+        </Table>
+      </div>
+    );
   }
 
   return ParticipantListPage;
