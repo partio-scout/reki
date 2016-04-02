2e0e95b03e316f5b1ee83bc1c170d908a37d955f
Leo Lahti <leo.lahti1@gmail.com>
2016-04-02 15:56:33 +0300

    Changed participant.js from model hook to operation hook

--- src/common/models/participant.js
+++ src/common/models/participant.js
@@ -2,36 +2,53 @@ import Promise from 'bluebird';
 import app from '../../server/server';
 
 export default function (Participant) {
-  Participant.afterInitalize = function() {
-    if (this.inCamp == false) {
-      const presenceHistoryRelation = this.presenceHistory;
-      const create = Promise.promisify(presenceHistoryRelation.create, { context: presenceHistoryRelation } );
-      create( { departed: null } )
-        .then(result => result.participant(this))
-        .catch(err => { console.error('There was an error', err); return;});
-    }
-  };
-  Participant.afterUpdate = function(next) {
-    const presenceHistoryRelation = this.presenceHistory;
-    const presenceHistory = Promise.promisify(this.presenceHistory, { context: this } );
-    const create = Promise.promisify(presenceHistoryRelation.create, { context: presenceHistoryRelation } );
-    const updateAll = Promise.promisify(app.models.participantHistory.updateAll, { context: app.models.participantHistory } );
-    const isInCamp = 2;
-    const leftCampTemporarily = 1;
-    const notInCamp = 0;
-      //check if latest relation doesn't have arrived value set
-    presenceHistory( { fields: { id:true, departed:false, arrived:false },  where: { arrived:null } } )
-      .then(result => {
-        if (this.inCamp == isInCamp) {
-          return updateAll( { where: { id: { inq: result.map( id => id.id ) } } } , { arrived:new Date().toISOString() } );
-        } else if (this.inCamp == notInCamp || this.inCamp == leftCampTemporarily) {
-        //check if latest relation has arrived value set
-          if (result.length == 0 ) {
-            return create( { departed: new Date().toISOString() } )
-              .then(result => result.participant(this));
+  Participant.observe('after save', (ctx, next) => {
+
+    const ParticipantHistory = app.models.ParticipantHistory;
+    const updateAllPresence = Promise.promisify(ParticipantHistory.updateAll, { context: ParticipantHistory } );
+    if (ctx.instance) {
+      const participant = ctx.instance;
+      const presenceHistoryRelation = participant.presenceHistory;
+      const createPresence = Promise.promisify(presenceHistoryRelation.create, { context: presenceHistoryRelation } );
+      const isInCamp = 2;
+      const leftCampTemporarily = 1;
+      const notInCamp = 0;
+      participant.presenceHistory( { fields: { id:true, departed:false, arrived:false } } )
+        .then(result => {
+          const presence = result.filter(presence => presence.arrived == null);
+          if (participant.inCamp == isInCamp && presence.length == 1) {
+            return updateAllPresence( { where: { id: { inq: presence.map( id => id.id ) } } } , { arrived:new Date().toISOString() } );
+          } else if (participant.inCamp == notInCamp || participant.inCamp == leftCampTemporarily) {
+            if (presence.length == 0 ) {
+              return createPresence( { departed: new Date().toISOString() } )
+                .then(result => result.participant(participant));
+            }
           }
-        }
-      })
-      .asCallback(next);
-  };
+        }).then(next);
+    } else {
+      const Participant = app.models.Participant;
+      const findParticipant = Promise.promisify(Participant.find, { context: Participant } );
+      const findPresence = Promise.promisify(ParticipantHistory.find, { context: ParticipantHistory } );
+      const isInCamp = 2;
+      const leftCampTemporarily = 1;
+      const notInCamp = 0;
+      findParticipant( ctx.where ).then(participants => {
+        participants.forEach( (participant, index, array) => {
+          const presenceHistoryRelation = participant.presenceHistory;
+          const createPresence = Promise.promisify(presenceHistoryRelation.create, { context: presenceHistoryRelation } );
+          findPresence( { where:{ participantId: participant.participantId } } ).then( result => {
+            const presence = result.filter(presence => presence.arrived == null);
+            if (participant.inCamp == isInCamp) {
+              return updateAllPresence( { id: { inq: presence.map( id => id.id ) } }  , { arrived:new Date().toISOString() } );
+            } else if (participant.inCamp == notInCamp || participant.inCamp == leftCampTemporarily) {
+              if (presence.length == 0 ) {
+                return createPresence( { departed: new Date().toISOString() } )
+                  .then(presence => presence.participant(participant));
+              }
+            }
+          });
+        });
+      }).then(next);
+    }
+  });
 }
--- test/presence.js
+++ test/presence.js
@@ -1,33 +1,57 @@
-import assert from 'assert';
-import app from '../src/server/server.js';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import app from '../src/server/server';
 import Promise from 'bluebird';
+
+const expect = chai.expect;
+
 describe('ParticipantModel', () => {
-  const db = app.models.Participant;
-  const create = Promise.promisify(db.create, { context: db });
-  const presenceHistory = Promise.promisify(db.presenceHistory, { context: db });
-  const updateAttribute = Promise.promisify(db.updateAttribute, { context: db });
-  const participant = create( { firstName: 'Matti', lastName:'Meikäläinen', nonScout: false, inCamp: false } )
-    .catch(result => result)
-    .catch(err => err);
-  it('Should have only one presenceHistory', () => {
-    updateAttribute( { where: { id: participant.participantid } }, { inCamp: false } ).catch(err => err);
-    assert.equal(participant.presenceHistory().then(result => result).length, 1);
-    updateAttribute( { where: { id: participant.participantid } }, { inCamp: true } ).catch(err => err);
-    assert.equal(participant.presenceHistory().then(result => result).length, 1);
-    updateAttribute( { where: { id: participant.participantid } }, { inCamp: false } ).catch(err => err);
-    assert.equal(participant.presenceHistory().then(result => result).length, 2);
-    updateAttribute( { where: { id: participant.participantid } }, { inCamp: true } ).catch(err => err);
-    assert.equal(participant.presenceHistory().then(result => result).length, 2);
+  chai.use(chaiAsPromised);
+
+  const Participant = app.models.Participant;
+  const PresenceHistory = app.models.ParticipantHistory;
+  const updateAllParticipants= Promise.promisify(Participant.updateAll, { context: Participant } );
+  const createParticipant = Promise.promisify(Participant.create, { context: Participant } );
+  const destroyAllParticipants = Promise.promisify(Participant.destroyAll, { context: Participant } );
+  const destroyAllPresence = Promise.promisify(PresenceHistory.destroyAll, { context: PresenceHistory } );
+  const findPresence = Promise.promisify(PresenceHistory.find, { context: PresenceHistory } );
+  const findParticipant = Promise.promisify(Participant.find, { context: Participant } );
+  let id;
+  beforeEach( () => createParticipant( { firstName: 'Raimo', lastName:'Vormisto', nonScout: false, inCamp: 0 } ).then(r => id = r.participantId));
+
+  afterEach( () => {
+    const presence = findParticipant( { where: { participantId: id }, fields: { participantid: true } } )
+    .then(result => destroyAllPresence( { participantid: result.participantid } ));
+    const participant = destroyAllParticipants( { participantId: id } );
+    return Promise.all([presence, participant]);
   });
-  it('Should have only one history with arrived null', () => {
-    updateAttribute( { where: { id: participant.participantid } }, { inCamp: true } ).catch(err => err);
-    for (let i = 0, len = 10; i < len; i++) {
-      updateAttribute( { where: { id: participant.participantid } }, { inCamp: false } ).catch(err => err);
-      updateAttribute( { where: { id: participant.participantid } }, { inCamp: true } ).catch(err => err);
-      assert.equal(participant.presenceHistory().then(result => result).length, i+2);
-    }
-    updateAttribute( { where: { id: participant.participantid } }, { inCamp: false } ).catch(err => err);
-    assert.equal(participant.presenceHistory().then(result => result).length, 12);
-    assert.equal(presenceHistory( { where: { arrived: null } } ).then(result => result).length, 1);
+
+  it('Should have only one presenceHistory', () => {
+    const update1 = updateAllParticipants( { participantId: id }, { inCamp: 1 } );
+    const update2 = updateAllParticipants( { participantId: id }, { nonScout: true } );
+    const update3 = updateAllParticipants( { participantId: id }, { nonScout: false } );
+    const update4 = updateAllParticipants( { participantId: id }, { inCamp: 2 } );
+    return Promise.all([update1, update2, update3, update4]).delay(200)
+      .then(() => findParticipant( { where: { participantId: id,  inCamp: 2 } }))
+      .then(participant => expect(findPresence( { where: { participantid: participant.participantid } } )).to.eventually.have.lengthOf(1))
+      .then(() => findParticipant( { where: { participantId: id,  inCamp: 2 } }))
+      .then(participant => expect(findPresence( { where: {  participantid: participant.participantid } } ).then(r => r.pop().arrived)).to.eventually.not.equal(null));
+  });
+
+  it('Should have multiple presenceHistories', () => {
+    const update1 = updateAllParticipants( { participantId: id }, { inCamp: 1 } );
+    const update2 = updateAllParticipants( { participantId: id }, { inCamp: 2 } );
+    return Promise.all([update1, update2]).delay(100).then(() =>  {
+      const update3 = updateAllParticipants( { participantId: id }, { inCamp: 1 } );
+      const update4 = updateAllParticipants( { participantId: id }, { inCamp: 2 } );
+      const update5 = updateAllParticipants( { participantId: id }, { inCamp: 2 } );
+      return Promise.all([update3, update4, update5]).delay(50)
+          .then(() => findParticipant( { where: { participantId: id,  inCamp: 2 } }))
+          .then(participant => expect(findPresence( { where: { participantid: participant.participantid } } )).to.eventually.have.lengthOf(2))
+          .then(() => findParticipant( { where: { participantId: id,  inCamp: 2 } }))
+          .then(participant => expect(findPresence( { where: {  participantid: participant.participantid } } ).then(r => r[0].arrived)).to.eventually.not.equal(null))
+          .then(() => findParticipant( { where: { participantId: id,  inCamp: 2 } }))
+          .then(participant => expect(findPresence( { where: {  participantid: participant.participantid } } ).then(r => r[1].arrived)).to.eventually.not.equal(null));
+    });
   });
 });
