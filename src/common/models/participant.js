0191f295b3483d0b059aa2ebb0aeb271edd64dcb
Ville Vuorenmaa <villevuor@gmail.com>
2016-04-17 15:52:36 +0300

    Hook to participant fetch query

--- src/common/models/participant.js
+++ src/common/models/participant.js
@@ -21,6 +21,47 @@ export default function (Participant) {
     .asCallback(next);
   });
 
+  Participant.beforeRemote('find', (ctx, participantInstance, next) => {
+
+    let filter = JSON.parse(ctx.args.filter);
+
+    // if multiple filters
+    if(filter.where['and'] != undefined) {
+
+      filter.where['and'].map(function(value, index, ar) {
+        if(value.textSearch != undefined && value.textSearch.length > 0) {
+          let name = {};
+          name.or = new Array();
+
+          name.or.push({ firstName: { like: '%' + value.textSearch + '%' } });
+          name.or.push({ lastName: { like: '%' + value.textSearch + '%' } });
+  
+          filter.where['and'].splice(index,1);
+          filter.where['and'].push(name);
+        }
+      });
+
+    } else if(filter.where.textSearch != undefined && filter.where.textSearch.length > 0) {
+      
+      const textSearchString = filter.where.textSearch;
+
+      delete filter.where.textSearch;
+
+      filter.where.or = new Array();
+      filter.where.or.push({ firstName: { like: '%' + textSearchString + '%' } });
+      filter.where.or.push({ lastName: { like: '%' + textSearchString + '%' } });
+
+    }
+  
+    ctx.args.filter = JSON.stringify(filter);
+
+    // console.log(ctx.args.filter);
+
+    next();
+
+  });
+
+
   Participant.observe('before delete', (ctx, next) => {
     const findParticipant = Promise.promisify(app.models.Participant.find, { context: app.models.Participant });
     if (ctx.instance) {
