5c2c8c654aab120fb80034b7f86010b78f0672ad
Miika Hänninen <miika.hanninen@reaktor.fi>
2016-05-07 20:34:05 +0300

    Add required changes for testing kuksa api to vagrantfile

---
+++ vagrant/set-env.sh
@@ -0,0 +1,4 @@
+export KUKSA_API_ENDPOINT=https://demo.kehatieto.fi:3300/partiolaiset/Leirirekisteri_Rajapinta/Api
+export KUKSA_API_USERNAME=sp_leirirekisteri
+export KUKSA_API_PASSWORD=Qutraj7Q\!
+export KUKSA_API_EVENTID=2e410c58-0be8-4004-af37-90e57f6cd6eb
