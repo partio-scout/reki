368b2d556f73f21e5b0c5b392f4240bf1f5d9113
Ville Vuorenmaa <villevuor@gmail.com>
2016-05-23 21:46:58 +0300

    Presence camp values updated
    Values were changed at 44d7bbe0

--- test/presence-creation-test.js
+++ test/presence-creation-test.js
@@ -29,27 +29,27 @@ describe('Participant change event', () => {
   });
 
   it('Should have only one presenceHistory', () => {
-    const update1 = updateAllParticipants({ participantId: id }, { inCamp: 1 });
+    const update1 = updateAllParticipants({ participantId: id }, { inCamp: 2 });
     return Promise.all([update1])
       .then(() => {
         const update2 = updateAllParticipants({ participantId: id }, { nonScout: true });
         const update3 = updateAllParticipants({ participantId: id }, { nonScout: false });
-        const update4 = updateAllParticipants({ participantId: id }, { inCamp: 2 });
+        const update4 = updateAllParticipants({ participantId: id }, { inCamp: 3 });
         return Promise.all([update2, update3, update4])
-          .then(() => findParticipant({ where: { participantId: id,  inCamp: 2 } }))
+          .then(() => findParticipant({ where: { participantId: id,  inCamp: 3 } }))
           .then(participant => expect(findPresence({ where:
                                                    { participantid: participant.participantid } }))
                                       .to.eventually.have.lengthOf(1))
-          .then(() => findParticipant({ where: { participantId: id,  inCamp: 2 } }))
+          .then(() => findParticipant({ where: { participantId: id,  inCamp: 3 } }))
           .then(participant => expect(findPresence({ where: {  participantid: participant.participantid } })
                                       .then(r => r.pop().arrived)).to.eventually.not.equal(null));
       });
   });
 
-  it('Should have multiple presenceHistories', () => updateAllParticipants({ participantId: id }, { inCamp: 0 })
-      .then(() => updateAllParticipants({ participantId: id }, { inCamp: 2 })
-          .then(() => updateAllParticipants({ participantId: id }, { inCamp: 0 })
-            .then(() => updateAllParticipants({ participantId: id }, { inCamp: 2 }))
+  it('Should have multiple presenceHistories', () => updateAllParticipants({ participantId: id }, { inCamp: 1 })
+      .then(() => updateAllParticipants({ participantId: id }, { inCamp: 3 })
+          .then(() => updateAllParticipants({ participantId: id }, { inCamp: 1 })
+            .then(() => updateAllParticipants({ participantId: id }, { inCamp: 3 }))
       )
     ).then(() => expect(findPresence({ participantid: id })).to.eventually.have.lengthOf(2))
   );
