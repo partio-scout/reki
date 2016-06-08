f82dbe79be7572d1de91eaf1b5d53e2909c0b564
Ville Vuorenmaa <villevuor@gmail.com>
2016-06-08 20:06:20 +0300

    Updated test, failing

--- test/massedit-endpoint-test.js
+++ test/massedit-endpoint-test.js
@@ -10,76 +10,63 @@ const expect = chai.expect;
 chai.use(chaiAsPromised);
 
 describe('Participant mass edit endpoint test', () => {
-  let adminuserId, accessToken;
+  const testParticipants = [
+    {
+      'participantId': 1,
+      'firstName': 'Teemu',
+      'lastName': 'Testihenkilö',
+      'nonScout': false,
+      'localGroup': 'Testilippukunta',
+      'campGroup': 'Leirilippukunta',
+      'subCamp': 'Alaleiri',
+      'ageGroup': 'sudenpentu',
+      'memberNumber': 123,
+      'inCamp': 0,
+    },
+    {
+      'participantId': 2,
+      'firstName': 'Tero',
+      'lastName': 'Esimerkki',
+      'nonScout': false,
+      'localGroup': 'Testilippukunta',
+      'campGroup': 'Leirilippukunta',
+      'subCamp': 'Alaleiri',
+      'ageGroup': 'sudenpentu',
+      'memberNumber': 345,
+      'inCamp': 0,
+    },
+    {
+      'participantId': 3,
+      'firstName': 'Jussi',
+      'lastName': 'Jukola',
+      'nonScout': false,
+      'localGroup': 'Testilippukunta',
+      'campGroup': 'Leirilippukunta',
+      'subCamp': 'Alaleiri',
+      'ageGroup': 'seikkailija',
+      'memberNumber': 859,
+      'inCamp': 0,
+    },
+  ];
 
-  const testParticipants = new Array();
+  const adminUserFixture = {
+    'username': 'testAdmin',
+    'memberNumber': '7654321',
+    'email': 'testi@adm.in',
+    'password': 'salasana',
+    'phone': 'n/a',
+    'firstName': 'Testi',
+    'lastName': 'Admin',
+  };
 
-  testParticipants.push({
-    'participantId': 1,
-    'firstName': 'Teemu',
-    'lastName': 'Testihenkilö',
-    'nonScout': false,
-    'localGroup': 'Testilippukunta',
-    'campGroup': 'Leirilippukunta',
-    'subCamp': 'Alaleiri',
-    'ageGroup': 'sudenpentu',
-    'memberNumber': 123,
-    'inCamp': 0,
-  });
-  testParticipants.push({
-    'participantId': 2,
-    'firstName': 'Tero',
-    'lastName': 'Esimerkki',
-    'nonScout': false,
-    'localGroup': 'Testilippukunta',
-    'campGroup': 'Leirilippukunta',
-    'subCamp': 'Alaleiri',
-    'ageGroup': 'sudenpentu',
-    'memberNumber': 345,
-    'inCamp': 0,
-  });
-  testParticipants.push({
-    'participantId': 3,
-    'firstName': 'Jussi',
-    'lastName': 'Jukola',
-    'nonScout': false,
-    'localGroup': 'Testilippukunta',
-    'campGroup': 'Leirilippukunta',
-    'subCamp': 'Alaleiri',
-    'ageGroup': 'seikkailija',
-    'memberNumber': 859,
-    'inCamp': 0,
-  });
+  let accessToken = null;
 
   beforeEach(() =>
-    resetDatabase().then(() =>
-      testUtils.createFixture('RegistryUser', {
-        'username': 'testAdmin',
-        'memberNumber': '7654321',
-        'email': 'testi@adm.in',
-        'password': 'salasana',
-        'phone': 'n/a',
-        'firstName': 'Testi',
-        'lastName': 'Admin',
-      })
-    ).then(adminUser => {
-      adminuserId = adminUser.id;
-      return testUtils.createFixture('Role', {
-        name: 'admin',
-      });
-    }).then(role =>
-      testUtils.createFixture('RoleMapping', {
-        principalType: 'USER',
-        principalId: adminuserId,
-        roleId: role.id,
-      })
-    ).then(() =>
-      testUtils.loginUser('testAdmin')
-    ).then(newAccessToken =>
-      accessToken = newAccessToken
-    ).then(() =>
-      testUtils.createFixture('Participant', testParticipants)
-    )
+    resetDatabase()
+    .then(() => testUtils.createUserWithRoles(['admin'], adminUserFixture))
+    .then(() => testUtils.createFixture('Participant', testParticipants))
+    .then(() => testUtils.loginUser(adminUserFixture.username, adminUserFixture.password))
+    .then(newAccessToken => accessToken = newAccessToken.id)
   );
 
   function expectParticipantInCampValues(expectedResult, response) {
@@ -95,28 +82,22 @@ describe('Participant mass edit endpoint test', () => {
 
   function postInstanceToDb(modelInPlural, changes, accessToken) {
     return request(app)
-      .post(`/api/${modelInPlural}?access_token=${accessToken.id}`)
+      .post(`/api/${modelInPlural}?access_token=${accessToken}`)
       .send(changes)
       .expect(200);
   }
 
   it('Should update right rows', () =>
     postInstanceToDb('participants/update', { ids: [1,2], newValue: 3, fieldName: 'inCamp' }, accessToken)
-    .then( () =>
-      queryParticipants(accessToken)
-      .then(res =>
-        expectParticipantInCampValues([ 3, 3, 0 ], res.body)
-      )
+      .then( () => queryParticipants(accessToken)
+      .then( res => expectParticipantInCampValues([ 3, 3, 0 ], res.body) )
     )
   );
 
   it('Should not update wrong fields', () =>
     postInstanceToDb('participants/update', { ids: [1,2], newValue: 3, fieldName: 'subCamp' }, accessToken)
-    .then( () =>
-      queryParticipants(accessToken)
-      .then(res =>
-        expectParticipantInCampValues([ 0, 0, 0 ], res.body)
-      )
+      .then( () => queryParticipants(accessToken)
+      .then( res => expectParticipantInCampValues([ 0, 0, 0 ], res.body) )
     )
   );
 
