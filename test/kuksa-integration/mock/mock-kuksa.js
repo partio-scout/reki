4906f5a714b8879ea4dca6621cf7261c754baec6
Emil Virkki <emil.virkki@gmail.com>
2016-06-13 10:07:45 +0300

    Implement mock Kuksa

---
+++ test/kuksa-integration/mock/mock-kuksa.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+
+const app = express();
+const PORT = 4001;
+const password = 'password';
+const username = 'username';
+const eventid = 'c3ef7c48-1df4-4bf8-b1d0-d523d601a3e1';
+const endpoint = `http://localhost:${PORT}`;
+let server;
+
+function serveFixtures(type) {
+  const collections = fs.readdirSync(path.resolve(__dirname, `../fixtures/${type}`));
+  collections.forEach(collection => {
+    app.get(`/${collection}`, (req, res) => {
+      if (req.query.Guid !== eventid) {
+        res.status(400).send('Wrong or no event id given');
+        console.error('Wrong or no event id given', req.query);
+      } else {
+        res.set('Content-Type', 'application/json');
+        const file = path.resolve(__dirname, `../fixtures/${type}/${collection}`);
+        res.send(fs.readFileSync(file));
+        console.log('OK:', req.path);
+      }
+    });
+  });
+}
+
+const mock = {
+  password: password,
+  username: username,
+  eventid: eventid,
+  endpoint: endpoint,
+  start: () => {
+    server = app.listen(PORT);
+    return server;
+  },
+  stop: () => server.close(),
+  serveFixtures: serveFixtures,
+};
+
+export default mock;
+
+if (require.main === module) {
+  mock.serveFixtures('all');
+  mock.start();
+  console.log(`Mock Kuksa running at endpoint ${endpoint}`)
+}
