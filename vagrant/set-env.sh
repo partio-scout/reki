ac29955ba25db054529ee73873a94a8683641095
Emil Virkki <emil.virkki@gmail.com>
2016-06-21 01:02:36 +0300

    Use mock instead of real Demo-Kuksa in dev

--- vagrant/set-env.sh
+++ vagrant/set-env.sh
@@ -1,4 +1,4 @@
-export KUKSA_API_ENDPOINT=https://demo.kehatieto.fi:3300/partiolaiset/Leirirekisteri_Rajapinta/Api
-export KUKSA_API_USERNAME=sp_leirirekisteri
-export KUKSA_API_PASSWORD=Qutraj7Q\!
-export KUKSA_API_EVENTID=2e410c58-0be8-4004-af37-90e57f6cd6eb
+export KUKSA_API_ENDPOINT=http://localhost:4001
+export KUKSA_API_USERNAME=username
+export KUKSA_API_PASSWORD=password
+export KUKSA_API_EVENTID=c3ef7c48-1df4-4bf8-b1d0-d523d601a3e1
