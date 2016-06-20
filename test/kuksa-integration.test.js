40a848ee10727209ee996c6e40018cff295ff76a
Emil Virkki <emil.virkki@gmail.com>
2016-06-21 00:40:14 +0300

    Add functional smoke test for Kuksa integration

---
+++ test/kuksa-integration.test.js
@@ -0,0 +1,42 @@
+import app from '../src/server/server';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { resetDatabase } from '../scripts/seed-database';
+import mockKuksa from './kuksa-integration/mock/mock-kuksa';
+import { exec } from 'child_process';
+import Promise from 'bluebird';
+import _ from 'lodash';
+
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+
+describe('Kuksa integration', () => {
+  const countParticipants = Promise.promisify(app.models.Participant.count, { context: app.models.Participant });
+
+  before(function(done) {
+    this.timeout(20000);
+    return resetDatabase().then(() => {
+      mockKuksa.serveFixtures('all');
+      mockKuksa.start();
+
+      const options = {
+        cwd: process.CWD,
+        env: _.merge(process.env, {
+          KUKSA_API_ENDPOINT: mockKuksa.endpoint,
+          KUKSA_API_USERNAME: mockKuksa.username,
+          KUKSA_API_PASSWORD: mockKuksa.password,
+          KUKSA_API_EVENTID: mockKuksa.eventid,
+        }),
+      };
+      exec('npm run fetch-from-kuksa', options, () => { console.log('jee'); done(); });
+    });
+  });
+
+  it('produces the expected amount of results in the database', () => {
+    expect(countParticipants()).to.eventually.equal(53);
+  });
+
+  after(() => {
+    mockKuksa.stop();
+  });
+});
