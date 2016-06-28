c0949a50b4c850e605d522843edd110eddb5b665
Ville Vuorenmaa <villevuor@gmail.com>
2016-06-28 21:08:53 +0300

    Content disposition header
    Disallows api seeking

--- src/server/boot/04-rest-api.js
+++ src/server/boot/04-rest-api.js
@@ -1,4 +1,12 @@
 export default function mountRestApi(server) {
+  
   const restApiRoot = server.get('restApiRoot');
+  
+  server.use(restApiRoot, function(req, res, next) {
+    res.setHeader('Content-Disposition', 'attachment; filename="rest.json"');
+    return next();
+  });
+  
   server.use(restApiRoot, server.loopback.rest());
+  
 }
