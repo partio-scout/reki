39809b6fe82c3e7fc2b3ee8831ec8678df1d8ba7
Pompo Stenberg <pompo.stenberg@solita.fi>
2016-06-28 18:57:49 +0300

    Made forceSsl const instead of var

--- src/server/server.js
+++ src/server/server.js
@@ -14,7 +14,7 @@ app.start = function() {
   });
 };
 
-var forceSsl = function (req, res, next) {
+const forceSsl = function (req, res, next) {
   if (req.headers['x-forwarded-proto'] !== 'https') {
     return res.redirect(['https://', req.get('Host'), req.url].join(''));
   }
