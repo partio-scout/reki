9b89d59385ca9654573ab8630c55bd34edfb59e6
Matias Turunen <mt95@windowslive.com>
2016-07-12 21:00:47 +0300

    Add script to create accesstoken to roihuapp

---
+++ scripts/create-roihuapp-token.js
@@ -0,0 +1,63 @@
+import Promise from 'bluebird';
+import app from '../src/server/server.js';
+import crypto from 'crypto';
+
+const lifeTime = process.argv[2];
+if (!lifeTime) {
+  console.log('You must specify token lifetime in seconds - node create-roihuapp-token.js <lifetime>');
+  process.exit(1);
+}
+
+const {
+  RegistryUser,
+  Role,
+  RoleMapping,
+} = app.models;
+
+const createUser = Promise.promisify(RegistryUser.create, { context: RegistryUser });
+const findUser = Promise.promisify(RegistryUser.findOne, { context: RegistryUser });
+const findRole = Promise.promisify(Role.findOne, { context: Role });
+const createRoleMapping = Promise.promisify(RoleMapping.create, { context: RoleMapping });
+
+function addRolesToUser(userId, rolename) {
+  return findRole({ where: { name: rolename } })
+    .then(role => createRoleMapping({
+      'principalType': 'USER',
+      'principalId': userId,
+      'roleId': role.id,
+    }));
+}
+
+function findOrCreateAppUser(name) {
+  return findUser({ where: { username: name } })
+  .then(user => {
+    if (user) {
+      return user;
+    } else {
+      return createUser({
+        firstName: 'roihuapp',
+        lastName: 'roihuapp',
+        username: name,
+        email: 'not_actual_roihuapp_email@roihu2016.fi',
+        password: crypto.randomBytes(24).toString('hex'),
+        lastModified: new Date(),
+      })
+      .catch(err => console.log(err));
+    }
+  });
+}
+
+findOrCreateAppUser('roihuapp-user')
+.then(user => {
+  addRolesToUser(user.id, 'roihuapp')
+  .then(() => {
+    user.createAccessToken(lifeTime, (err, accessToken) => {
+      if (err) {
+        console.error('Can\'t generate access token:', err);
+      } else {
+        console.log(`Accesstoken: ${accessToken.id} lifetime ${lifeTime} seconds`);
+        process.exit(0);
+      }
+    });
+  });
+});
