80cc9b23978d6e88f2548268c2c2d7839e232072
ritvje <jenna.ritvanen@gmail.com>
2016-07-16 00:07:43 +0300

    Promisify script

--- scripts/set-password.js
+++ scripts/set-password.js
@@ -1,33 +1,33 @@
 import Promise from 'bluebird';
 import app from '../src/server/server.js';
-const RegistryUser = app.models.RegistryUser;
-const findOne = Promise.promisify(RegistryUser.findOne, { context: RegistryUser });
+const findUser = Promise.promisify(app.models.RegistryUser.findOne, { context: app.models.RegistryUser });
 
 const opts = require('commander')
   .usage('<email of the user to set password> <password>')
   .parse(process.argv);
+
 if (opts.args.length < 1) {
-  opts.outputHelp();
   console.error('Please specify the user to set password.');
-  process.exit(1);
-}else if (opts.args.length < 2) {
-  opts.outputHelp();
-  console.error('Please insert password.');
-  process.exit(1);
+  opts.help();
+} else if (opts.args.length < 2) {
+  console.error('Please specify password.');
+  opts.help();
 }
-findOne({ where: { email: opts.args[0] } })
+
+findUser({ where: { email: opts.args[0] } })
   .then(user => {
-    user.updateAttribute('password', opts.args[1], (err => {
-      if (err) {
-        console.log(err);
-        process.exit(1);
-      } else {
-        console.log('password set');
-        process.exit(0);
-      }
-    }));
+    if (user) {
+      const updateUser = Promise.promisify(user.updateAttribute, { context: user });
+      return updateUser('password', opts.args[1]);
+    } else {
+      throw new Error(`Couldn't find user with email ${opts.args[0]}!`);
+    }
+  })
+  .then(() => {
+    console.log(`Password set for user ${opts.args[0]}.`);
+    process.exit(0);
   })
   .catch(err => {
-    console.log(err);
+    console.error(err);
     process.exit(1);
   });
