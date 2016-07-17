27aadedce74ff6825a3d121f061527699a699393
Miika Hänninen <miika.hanninen@reaktor.fi>
2016-07-17 13:07:17 +0300

    Move the directory module files for easier file searching
    Node.js will resolve xxx.js and xxx/index.js as the same file, so moving
    the files inside the directories makes it easier to browse directory listings.

--- src/client/actions.js
+++
@@ -1,5 +0,0 @@
-// Tämä tiedosto kokoaa kaikki actions-kansion tiedostot yhteen moduuliin
-
-export { getParticipantActions } from './actions/ParticipantActions';
-export { getRegistryUserActions } from './actions/RegistryUserActions';
-export { getSearchFilterActions } from './actions/SearchFilterActions';
---
+++ src/client/stores/index.js
@@ -0,0 +1,5 @@
+// Tämä tiedosto kokoaa kaikki stores-kansion tiedostot yhteen moduuliin
+
+export { getParticipantStore } from './ParticipantStore';
+export { getRegistryUserStore } from './RegistryUserStore';
+export { getSearchFilterStore } from './SearchFilterStore';
