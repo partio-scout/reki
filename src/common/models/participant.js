c4fcc08c0008d6504737d78d2c25761aa64adcaf
Ville Vuorenmaa <villevuor@gmail.com>
2016-07-20 10:57:03 +0300

    Fix date filter constructing when query with other filters

--- src/common/models/participant.js
+++ src/common/models/participant.js
@@ -35,7 +35,7 @@ export default function (Participant) {
 
       if (where.and) {
         dates = where.and.filter(filter => filter.dates);
-        dates = dates.dates;
+        dates = dates[0].dates;
         where.and = where.and.filter(filter => !filter.dates);
       } else {
         dates = where.dates;
