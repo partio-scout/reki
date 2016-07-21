26e426605638d68942dae464ca2e1b4d3e5c9681
Miika Hänninen <miika.hanninen@reaktor.fi>
2016-07-21 12:12:14 +0300

    Populate most quickfilters in create-fake-data.js

--- scripts/create-fake-data.js
+++ scripts/create-fake-data.js
@@ -8,6 +8,10 @@ const createParticipant = Promise.promisify(ParticipantModel.create, { context: 
 const ParticipantDateModel = app.models.ParticipantDate;
 const createParticipantDate = Promise.promisify(ParticipantDateModel.create, { context: ParticipantDateModel });
 
+const OptionModel = app.models.Option;
+const createOptions = Promise.promisify(OptionModel.create, { context: OptionModel });
+const removeOptions = Promise.promisify(OptionModel.destroyAll, { context: OptionModel });
+
 const opts = require('commander')
   .usage('<amount of fake participants to create>')
   .parse(process.argv);
@@ -20,6 +24,115 @@ if (opts.args.length > 1 || !parseInt(opts.args[0], 10) || opts.args[0] < 0) {
 const amountToCreate = opts.args.length == 1 ? opts.args[0] : 5;
 
 faker.locale = 'it';
+
+const localGroups = [
+  'Euran Peurat ry',
+  'Harjavallan Pirkat',
+  'Henrikin Tapulitytöt',
+  'Honkahukat ry',
+  'Huittisten Eränkävijät ry',
+  'Jokilaakson Ravut ry',
+  'Jämijärven Miilunvartijat ry',
+  'Kaavin Kiehiset',
+  'Kallan Kontiot',
+  'Kallan Tiirat',
+  'Kallan Tytöt',
+  'Kankaanpään Korpiveikot',
+  'Kannonkiertäjät',
+  'Karimo ry',
+  'Karvian Majavapartio ry',
+  'Keikyän Korvenkävijät ry',
+  'Keiteleen Saukot',
+  'Kiikan Leiripeikot',
+  'Kiikoisten Partiopojat',
+  'Kinahmin Kiipijät',
+  'Kirkkonummen Metsänkävijät',
+  'Kirkkonummen Metsäntytöt',
+  'Kirnun Kiertäjät',
+  'Kiukaisten Metsäveikot ry',
+  'Kiuru-Partio',
+  'Klapaset',
+  'Kopardit ry',
+  'Kuilun Kulkijat',
+  'Kuninkaantien Kipinät',
+  'Kuopion Eräversot',
+  'Kuopion Kurjenmiekat',
+  'Kuusiston Linnanyrjänät',
+  'Lahelan Palokärjet',
+  'Lapinjärven Peurat',
+  'Lappohjan Vasat',
+  'Leirisiskot',
+  'Linnajoen Partiolaiset',
+  'Lohjan Eräveikot',
+  'Lohjan Nummitytöt',
+  'Lohjanharjun Vartijat',
+  'Marttilan Martit',
+  'Mellilän Menninkäiset',
+  'Mustavuoren Sissit',
+  'Mäntsälän Metsäkävyt',
+  'Nummen Samoojat',
+  'Nuotiotytöt',
+  'Polaris',
+  'Pornaisten Solmu',
+  'Porvoon Metsänkävijät',
+  'Porvoon Polunlöytäjät',
+  'Pukkilan Partio',
+  'Puskapartiolaiset',
+  'Rajamäen Kekäleet',
+  'Tapiolan Metsänkävijät',
+  'Tavastit',
+  'Tellervoiset',
+  'Tikkurilan Siniset',
+  'Toimen Pojat - Unga Fribyggare',
+  'Toimen tytöt',
+  'Tornikotkat',
+  'Tuikku-tytöt',
+  'Turun Sinikotkat',
+  'Töölön Nuotioveikot',
+  'Töölön Siniset',
+  'Töölön Tähystäjät',
+  'Vaaran Vaeltajat',
+  'Vantaan Jokiversot',
+  'Vantaan Metsänkävijät',
+  'Vartiovuoren Pojat',
+  'Vartiovuoren Tytöt',
+  'Viestitytöt',
+  'Viherlaakson Peurat',
+];
+
+const campStructure = {
+  humina: {
+    'Rankki': ['Hautamäki', 'Väkiparta'],
+    'Ruuna': ['Holmisto', 'Kauranen'],
+  },
+  hurma: {
+    'Riimu': ['Aalto', 'Uhlenius'],
+  },
+  polte: {
+    'Rauha': ['Norokorpi', 'Hietanen'],
+  },
+  raiku: {
+    'Raukola': ['Eino', 'Jenni'],
+  },
+  riehu: {
+    'Rouhe': ['Lehto', 'Jylhä'],
+    'Revontuli': ['Lehto', 'Jylhä'],
+  },
+  syke: {
+    'Rantala': ['Warro', 'Toukonen'],
+  },
+  unity: {
+    'Ruukki': ['Finelli', 'Puntila'],
+  },
+};
+
+const ageGroups = [
+  'perheleiriläinen',
+  'tarpoja',
+  'samoaja',
+  'vaeltaja',
+  'aikuinen',
+];
 
 function countParticipants() {
   ParticipantModel.count((err, sum) => {
@@ -54,111 +167,10 @@ function randomElement(array) {
 }
 
 function getLocalGroup() {
-  const localGroups = [
-    'Euran Peurat ry',
-    'Harjavallan Pirkat',
-    'Henrikin Tapulitytöt',
-    'Honkahukat ry',
-    'Huittisten Eränkävijät ry',
-    'Jokilaakson Ravut ry',
-    'Jämijärven Miilunvartijat ry',
-    'Kaavin Kiehiset',
-    'Kallan Kontiot',
-    'Kallan Tiirat',
-    'Kallan Tytöt',
-    'Kankaanpään Korpiveikot',
-    'Kannonkiertäjät',
-    'Karimo ry',
-    'Karvian Majavapartio ry',
-    'Keikyän Korvenkävijät ry',
-    'Keiteleen Saukot',
-    'Kiikan Leiripeikot',
-    'Kiikoisten Partiopojat',
-    'Kinahmin Kiipijät',
-    'Kirkkonummen Metsänkävijät',
-    'Kirkkonummen Metsäntytöt',
-    'Kirnun Kiertäjät',
-    'Kiukaisten Metsäveikot ry',
-    'Kiuru-Partio',
-    'Klapaset',
-    'Kopardit ry',
-    'Kuilun Kulkijat',
-    'Kuninkaantien Kipinät',
-    'Kuopion Eräversot',
-    'Kuopion Kurjenmiekat',
-    'Kuusiston Linnanyrjänät',
-    'Lahelan Palokärjet',
-    'Lapinjärven Peurat',
-    'Lappohjan Vasat',
-    'Leirisiskot',
-    'Linnajoen Partiolaiset',
-    'Lohjan Eräveikot',
-    'Lohjan Nummitytöt',
-    'Lohjanharjun Vartijat',
-    'Marttilan Martit',
-    'Mellilän Menninkäiset',
-    'Mustavuoren Sissit',
-    'Mäntsälän Metsäkävyt',
-    'Nummen Samoojat',
-    'Nuotiotytöt',
-    'Polaris',
-    'Pornaisten Solmu',
-    'Porvoon Metsänkävijät',
-    'Porvoon Polunlöytäjät',
-    'Pukkilan Partio',
-    'Puskapartiolaiset',
-    'Rajamäen Kekäleet',
-    'Tapiolan Metsänkävijät',
-    'Tavastit',
-    'Tellervoiset',
-    'Tikkurilan Siniset',
-    'Toimen Pojat - Unga Fribyggare',
-    'Toimen tytöt',
-    'Tornikotkat',
-    'Tuikku-tytöt',
-    'Turun Sinikotkat',
-    'Töölön Nuotioveikot',
-    'Töölön Siniset',
-    'Töölön Tähystäjät',
-    'Vaaran Vaeltajat',
-    'Vantaan Jokiversot',
-    'Vantaan Metsänkävijät',
-    'Vartiovuoren Pojat',
-    'Vartiovuoren Tytöt',
-    'Viestitytöt',
-    'Viherlaakson Peurat',
-  ];
-
   return randomElement(localGroups);
 }
 
 function getSubCampVillageAndCampGroup() {
-  const campStructure = {
-    humina: {
-      'Rankki': ['Hautamäki', 'Väkiparta'],
-      'Ruuna': ['Holmisto', 'Kauranen'],
-    },
-    hurma: {
-      'Riimu': ['Aalto', 'Uhlenius'],
-    },
-    polte: {
-      'Rauha': ['Norokorpi', 'Hietanen'],
-    },
-    raiku: {
-      'Raukola': ['Eino', 'Jenni'],
-    },
-    riehu: {
-      'Rouhe': ['Lehto', 'Jylhä'],
-      'Revontuli': ['Lehto', 'Jylhä'],
-    },
-    syke: {
-      'Rantala': ['Warro', 'Toukonen'],
-    },
-    unity: {
-      'Ruukki': ['Finelli', 'Puntila'],
-    },
-  };
-
   const subCamp = randomElement(Object.keys(campStructure));
   const village = randomElement(Object.keys(campStructure[subCamp]));
   const campGroup = randomElement(campStructure[subCamp][village]);
@@ -171,14 +183,6 @@ function getSubCampVillageAndCampGroup() {
 }
 
 function getAgeGroup() {
-  const ageGroups = [
-    'perheleiriläinen',
-    'tarpoja',
-    'samoaja',
-    'vaeltaja',
-    'aikuinen',
-  ];
-
   return randomElement(ageGroups);
 }
 
@@ -231,5 +235,33 @@ function addParticipantDate(participant) {
   return createParticipantDate({ participantId: participant.participantId, date: new Date(2016,6,22) });
 }
 
+function populateOptions() {
+  const localGroupOptions = localGroups.map(group => ({ property: 'localGroup', value: group }));
+  const campStructureOptions = createCampStructureOptions();
+  const ageGroupOptions = ageGroups.map(ageGroup => ({ property: 'ageGroup', value: ageGroup }));
+
+  const options = localGroupOptions
+    .concat(campStructureOptions)
+    .concat(ageGroupOptions);
+
+  createOptions(options);
+}
+
+function createCampStructureOptions() {
+  const options = [];
+  for (const subCamp in campStructure) {
+    options.push({ property: 'subCamp', value: subCamp });
+    for (const village in campStructure[subCamp]) {
+      options.push({ property: 'village', value: village });
+      for (const campGroup of campStructure[subCamp][village]) {
+        options.push({ property: 'campGroup', value: campGroup });
+      }
+    }
+  }
+  return options;
+}
+
 console.log(`Attempting to create ${amountToCreate} mock participants.`);
-createMockParticipants(amountToCreate);
+removeOptions()
+  .then(() => populateOptions())
+  .then(() => createMockParticipants(amountToCreate));
