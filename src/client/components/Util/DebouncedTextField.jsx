ddba895ca75ba973ceccdc65e07d700379d1665b
Masa <matti.sippola@aalto.fi>
2016-11-16 09:15:34 +0200

    Refactor text field search as a React class

--- src/client/components/Util/DebouncedTextField.jsx
+++ src/client/components/Util/DebouncedTextField.jsx
@@ -3,42 +3,53 @@ import _ from 'lodash';
 import { Input } from 'react-bootstrap';
 
 export function getDebouncedTextField() {
-  function debouncedTextField({ value, label, property, onChange }) {
-
-    const delayedOnChange = _.debounce(value => onChange(property, value), 1500);
-
-    function handleValueChanged(event) {
-      event.persist();
-      delayedOnChange(event.target.value);
+  class DebouncedTextField extends React.Component {
+    constructor(props) {
+      super(props);
+      this.delayedOnChange = _.debounce(
+        value => this.props.onChange(this.props.property, value), 1500);
+      this.handleValueChanged = this.handleValueChanged.bind(this);
+      this.handleFieldBlur = this.handleFieldBlur.bind(this);
+      this.disableEnter = this.disableEnter.bind(this);
+      this.state = { textSearch: props.value };
     }
 
-    function handleFieldBlur(event) {
+
+    handleValueChanged(event) {
       event.persist();
-      delayedOnChange.flush(event.target.value);
+      this.setState({ textSearch: event.target.value });
+      this.delayedOnChange(event.target.value);
     }
 
-    function disableEnter(event) {
+    handleFieldBlur(event) {
+      event.persist();
+      this.delayedOnChange.flush(event.target.value);
+    }
+
+    disableEnter(event) {
       if (event.key === 'Enter') {
         event.preventDefault();
         event.persist();
-        delayedOnChange.flush(event.target.value);
+        this.delayedOnChange.flush(event.target.value);
         return false;
       }
     }
 
-    return (
-      <div>
-        <Input type="text" label={ label } value={ value } onChange={ handleValueChanged } onBlur={ handleFieldBlur } onKeyPress={ disableEnter } />
-      </div>
-    );
+    render() {
+      return (
+        <div>
+          <Input type="text" label={ this.props.label } value={ this.state.textSearch } onChange={ this.handleValueChanged } onBlur={ this.handleFieldBlur } onKeyPress={ this.disableEnter } />
+        </div>
+      );
+    }
   }
 
-  debouncedTextField.propTypes = {
+  DebouncedTextField.propTypes = {
     value: React.PropTypes.any,
     label: React.PropTypes.string.isRequired,
     property: React.PropTypes.string.isRequired,
     onChange: React.PropTypes.func.isRequired,
   };
 
-  return debouncedTextField;
+  return DebouncedTextField;
 }
