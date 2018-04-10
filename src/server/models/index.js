61b2aa10e3e6c175b31248032b963bc647e900cb
Emil Virkki <emil.virkki@gmail.com>
2018-04-10 18:37:42 +0300

    Disable insecure Sequelize operator string aliases
    See http://docs.sequelizejs.com/manual/tutorial/querying.html#operators-security

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -4,6 +4,7 @@ import integrationModels from './kuksa-integration-models.js';
 const dbUrl = process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 const db = new Sequelize(dbUrl, {
   logging: false,
+  operatorsAliases: false,
 });
 
 export const sequelize = db;
