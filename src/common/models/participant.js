3f2cb1997379172165c19935a7d0ee887ce6311d
Emil Virkki <emil.virkki@gmail.com>
2018-05-03 23:14:31 +0300

    Remove auditing for events that don't happen
    Currently the API doesn't support performing these actions, so there's no point in including them.

--- src/common/models/participant.js
+++ src/common/models/participant.js
@@ -1,6 +1,5 @@
 import Promise from 'bluebird';
 import app from '../../server/server';
-import loopback from 'loopback';
 import _ from 'lodash';
 
 export default function (Participant) {
@@ -207,24 +206,6 @@ export default function (Participant) {
     }
   }
 
-  Participant.afterRemote('create', (ctx, participantInstance, next) => {
-    const userId = ctx.req.accessToken ? ctx.req.accessToken.userId : 0;
-    app.models.AuditEvent.createEvent.Participant(userId, participantInstance.participantId, 'add')
-    .asCallback(next);
-  });
-
-  Participant.beforeRemote('findById', (ctx, participantInstance, next) => {
-    const userId = ctx.req.accessToken ? ctx.req.accessToken.userId : 0;
-    app.models.AuditEvent.createEvent.Participant(userId, ctx.req.params.id, 'find')
-    .asCallback(next);
-  });
-
-  Participant.beforeRemote('prototype.updateAttributes', (ctx, participantInstance, next) => {
-    const userId = ctx.req.accessToken ? ctx.req.accessToken.userId : 0;
-    app.models.AuditEvent.createEvent.Participant(userId, ctx.req.params.id, 'update')
-    .asCallback(next);
-  });
-
   Participant.beforeRemote('find', handleTextSearch);
   Participant.afterRemote('find', checkFullParticipantCount);
 
@@ -232,21 +213,6 @@ export default function (Participant) {
 
   Participant.beforeRemote('find', handleDateSearch);
   Participant.beforeRemote('count', handleDateSearch);
-
-  Participant.observe('before delete', (ctx, next) => {
-    const findParticipant = Promise.promisify(app.models.Participant.find, { context: app.models.Participant });
-    if (ctx.instance) {
-      const userId = loopback.getCurrentContext() ? loopback.getCurrentContext().get('accessToken').userId : 0;
-      app.models.AuditEvent.createEvent.Participant(userId, ctx.instance.participantId, 'delete')
-      .asCallback(next);
-    } else {
-      findParticipant({ where: ctx.where, fields: { participantId: true } })
-        .each(participant => {
-          const userId = (loopback.getCurrentContext() && loopback.getCurrentContext().get('accessToken')) ? loopback.getCurrentContext().get('accessToken').userId : 0;
-          return app.models.AuditEvent.createEvent.Participant(userId, participant.participantId, 'delete');
-        }).asCallback(next);
-    }
-  });
 
   Participant.massAssignField = (ids, fieldName, newValue, authorId) => {
     // field name : validation function
@@ -269,8 +235,11 @@ export default function (Participant) {
               authorId: authorId,
             });
           }
-
           row[fieldName] = newValue;
+
+          // TODO Test this audit event
+          app.models.AuditEvent.createEvent.Participant(authorId, row.id, 'update');
+
           return row.save();
         });
         return Promise.all(updates);
