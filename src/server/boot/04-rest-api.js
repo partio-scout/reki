2bc63bdd9149c6a58129a38a2573965138dfdf6a
Emil Virkki <emil.virkki@gmail.com>
2018-05-09 21:27:51 +0300

    Remove LoopBack REST API

--- src/server/boot/04-rest-api.js
+++ src/server/boot/04-rest-api.js
@@ -1,12 +1,6 @@
 export default function mountRestApi(server) {
-
-  const restApiRoot = server.get('restApiRoot');
-
-  server.use(restApiRoot, (req, res, next) => {
+  server.use('/api', (req, res, next) => {
     res.setHeader('Content-Disposition', 'attachment; filename="rest.json"');
     return next();
   });
-
-  server.use(restApiRoot, server.loopback.rest());
-
 }
