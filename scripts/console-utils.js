8a84f550ddc47987fd54c71f2115cb855752319f
Emil Virkki <emil.virkki@gmail.com>
2018-05-16 19:07:16 +0300

    Remove unneeded file

--- scripts/console-utils.js
+++
@@ -1,3 +0,0 @@
-const app = require('./src/server/server'); // eslint-disable-line no-unused-vars
-let res = '';
-const cb = function(err, result) { if (err) { console.error(err); } else { res = result; console.log(res); } };  // eslint-disable-line no-unused-vars
