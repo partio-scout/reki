362de058094bed86c7769641d687021d6f6c014a
Emil Virkki <emil.virkki@gmail.com>
2018-06-03 21:54:29 +0300

    Cleanup

--- webpack.development.config.babel.js
+++ webpack.development.config.babel.js
@@ -21,7 +21,6 @@ export default {
   },
   resolve: {
     extensions: [
-      //'',
       '.js',
       '.jsx',
     ],
--- webpack.production.config.babel.js
+++ webpack.production.config.babel.js
@@ -13,7 +13,6 @@ export default {
   },
   resolve: {
     extensions: [
-      //'',
       '.js',
       '.jsx',
     ],
