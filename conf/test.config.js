e2ff1e9942dd5378d9afe74953d6111ccad5222e
Panu Korpela <panu@vuolearning.fi>
2018-07-18 07:23:51 +0300

    First try at Kliffa 2018 configurations

--- conf/test.config.js
+++ conf/test.config.js
@@ -174,52 +174,26 @@ const customMultipleSelectionFields = [
 const participantDatesMapper = wrappedParticipant => {
   // Map payment names to arrays of dates when the participant is present
   const paymentToDatesMappings = {
-    'pe 15.7.': ['2016-07-15'],
-    'la 16.7.': ['2016-07-16'],
-    'su 17.7.': ['2016-07-17'],
-    'ma 18.7.': ['2016-07-18'],
-    'ti 19.7.': ['2016-07-19'],
+    '23.7.': ['2018-07-23'],
+    '24.7.': ['2018-07-24'],
+    '25.7.': ['2018-07-25'],
+    '26.7.': ['2018-07-26'],
+    '27.7.': ['2018-07-27'],
+    '28.7.': ['2018-07-28'],
+    '29.7.': ['2018-07-29'],
+    '30.7.': ['2018-07-30'],
+    '31.7.': ['2018-07-31'],
 
-    'ke 20.7.': ['2016-07-20'],
-    'ke 20.7. 100% alennus': ['2016-07-20'],
-    'ke 20.7. 50% alennus': ['2016-07-20'],
-
-    'to 21.7.': ['2016-07-21'],
-    'to 21.7. 100% alennus': ['2016-07-21'],
-    'to 21.7. 50% alennus': ['2016-07-21'],
-
-    'pe 22.7.': ['2016-07-22'],
-    'pe 22.7. 100% alennus': ['2016-07-22'],
-    'pe 22.7. 50% alennus': ['2016-07-22'],
-
-    'la 23.7.': ['2016-07-23'],
-    'la 23.7. 100% alennus': ['2016-07-23'],
-    'la 23.7. 50% alennus': ['2016-07-23'],
-
-    'su 24.7.': ['2016-07-24'],
-    'su 24.7. 100% alennus': ['2016-07-24'],
-    'su 24.7. 50% alennus': ['2016-07-24'],
-
-    'ma 25.7.': ['2016-07-25'],
-    'ma 25.7. 100% alennus': ['2016-07-25'],
-    'ma 25.7. 50% alennus': ['2016-07-25'],
-
-    'ti 26.7.': ['2016-07-26'],
-    'ti 26.7. 100% alennus': ['2016-07-26'],
-    'ti 26.7. 50% alennus': ['2016-07-26'],
-
-    'ke 27.7.': ['2016-07-27'],
-    'ke 27.7. 100% alennus': ['2016-07-27'],
-    'ke 27.7. 50% alennus': ['2016-07-27'],
-
-    'to 28.7.': ['2016-07-28'],
-    'pe 29.7.': ['2016-07-29'],
-    'la 30.7.': ['2016-07-30'],
-    'su 31.7.': ['2016-07-31'],
-
-    'Osallistun koko leirin ajaksi': ['2016-07-20','2016-07-21','2016-07-22','2016-07-23','2016-07-24','2016-07-25','2016-07-26','2016-07-27'],
-    'Osallistun koko purkuleirille (4 päivää) ja saan alennusta leirimaksusta 20 euroa. Summa hyvitetään purkuleirin jälkeen..': ['2016-07-28','2016-07-29','2016-07-30','2016-07-31'],
-    'Osallistun vain rakennus-/purkuleirille tai Home Hospitalityn isäntäperheenä.': [],
+    'Pitkä Kliffa 23.-30./31.7.2018 (seikkailijat ja tarpojat (10-15 -vuotiaat leiriläiset) 30.7. asti, sitä vanhemmat 31.7. asti)':
+        ['2018-07-23', '2018-07-24', '2018-07-25', '2018-07-26', '2018-07-27', '2018-07-28', '2018-07-29', '2018-07-30', '2018-07-31'],
+    'Pitkä Kliffa':
+      ['2018-07-23', '2018-07-24', '2018-07-25', '2018-07-26', '2018-07-27', '2018-07-28', '2018-07-29', '2018-07-30', '2018-07-31'],
+    'Lyhyt Kliffa (23.-26.7.2018)': ['2018-07-23', '2018-07-24', '2018-07-25', '2018-07-26'],
+    'Lyhyt Kliffa': ['2018-07-23', '2018-07-24', '2018-07-25', '2018-07-26'],
+    'Osallistumismaksu, 4-6 -vuotiaat lapset (pitkän tai lyhyen Kliffan ajan leireilevät)': [],
+    'Osallistumismaksu, 0-3 -vuotiaat lapset (ei sidoksissa leiriaikaan)': [],
+    'Osallistun päiväleiriläisenä ja valitsen läsnäolopäiväni alta': [],
+    'Participation fee': [],
   };
 
   return _(wrappedParticipant.getPayments())
