ae60e6802f67e3b09c76663bf16141a379ca6723
Miika Hänninen <miika.hanninen@reaktor.com>
2018-07-26 14:44:01 +0300

    Add other subcamps and totals for subcamp/agegroup

--- src/server/boot/03-presence-list.js
+++ src/server/boot/03-presence-list.js
@@ -9,9 +9,16 @@ export default function(app) {
     try {
       const totalCount = await models.Participant.count({ where: { presence: 3 } });
       const temporarilyAway = await models.Participant.count({ where: { presence: 2 } });
-      const rows = await sequelize.query(`SELECT "subCamp","ageGroup", COUNT(*) FROM participants WHERE presence='3' AND "subCamp" IN ('Triton', 'Gloria', 'Ra', 'Cyber') GROUP BY "subCamp", "ageGroup";`, { type: Sequelize.QueryTypes.SELECT });
+      const rows = await sequelize.query(`SELECT "subCamp","ageGroup", COUNT(*) FROM participants WHERE presence='3' GROUP BY "subCamp", "ageGroup";`, { type: Sequelize.QueryTypes.SELECT });
       const bySubCamp = _.groupBy(rows, row => row.subCamp);
+      const subCampNames = Object.keys(bySubCamp);
+      const otherSubCamps = _.difference(subCampNames, subCamps);
+      console.log(subCampNames, otherSubCamps);
 
+      const counts = _.map(subCamps, x => getSubcampCounts(bySubCamp, x));
+
+      const othersTotals = calculateAgeGroupSum(otherSubCamps.map(x => getSubcampCounts(bySubCamp, x)));
+      const ageGroupTotals = calculateAgeGroupSum([othersTotals, ...counts]);
       const html = `<!DOCTYPE html>
       <html lang="fi">
       <head>
@@ -33,29 +40,44 @@ export default function(app) {
       <th>Samoaja</th>
       <th>Vaeltaja</th>
       <th>Aikuinen</th>
+      <td>yhteensä alaleirissä</td>
       </tr>
-      ${_.flatMap(subCamps, subCamp => {
-        const subCampRows = bySubCamp[subCamp];
-
-        function getAgeGroup(ageGroup) {
-          const ageGroupObj = _.find(subCampRows, x => x.ageGroup === ageGroup) || {};
-          return ageGroupObj.count || 0;
-        }
-
-        return [
-          '<tr>',
-          `<th>${subCamp}</th>`,
-          `<td>${getAgeGroup('Lapsi 0-2v') + getAgeGroup('Lapsi 3-7v')}</td>`,
-          `<td>${getAgeGroup('Sudenpentu 7-9v')}</td>`,
-          `<td>${getAgeGroup('Seikkailija 10-12v')}</td>`,
-          `<td>${getAgeGroup('Tarpoja 12-15v')}</td>`,
-          `<td>${getAgeGroup('Samoaja 15-17v')}</td>`,
-          `<td>${getAgeGroup('Vaeltaja 18-22v')}</td>`,
-          `<td>${getAgeGroup('Aikuinen')}</td>`,
-          '</tr>',
-        ];
-      }).join('\n')
+      ${_.flatMap(counts, count => [
+        '<tr>',
+        `<th>${count.subCamp}</th>`,
+        `<td>${count.child}</td>`,
+        `<td>${count.supe}</td>`,
+        `<td>${count.seikkailija}</td>`,
+        `<td>${count.tarpoja}</td>`,
+        `<td>${count.samoaja}</td>`,
+        `<td>${count.vaeltaja}</td>`,
+        `<td>${count.adult}</td>`,
+        `<td>${count.total}</td>`,
+        '</tr>',
+      ]).join('\n')
       }
+      <tr>
+      <th>Muut</th>
+      <td>${othersTotals.child}</td>
+      <td>${othersTotals.supe}</td>
+      <td>${othersTotals.seikkailija}</td>
+      <td>${othersTotals.tarpoja}</td>
+      <td>${othersTotals.samoaja}</td>
+      <td>${othersTotals.vaeltaja}</td>
+      <td>${othersTotals.adult}</td>
+      <td>${othersTotals.total}</td>
+      </tr>
+      <tr>
+      <td>yhteensä ikäryhmää</td>
+      <td>${ageGroupTotals.child}</td>
+      <td>${ageGroupTotals.supe}</td>
+      <td>${ageGroupTotals.seikkailija}</td>
+      <td>${ageGroupTotals.tarpoja}</td>
+      <td>${ageGroupTotals.samoaja}</td>
+      <td>${ageGroupTotals.vaeltaja}</td>
+      <td>${ageGroupTotals.adult}</td>
+      <td></td>
+      </tr>
       </table>
       <p>Väliaikaisesti poissa: ${temporarilyAway}</p>
       </div>
@@ -67,5 +89,57 @@ export default function(app) {
     } catch (e) {
       next(e);
     }
+  });
+}
+
+function getSubcampCounts(bySubCamp, subCamp) {
+  const subCampRows = bySubCamp[subCamp];
+
+  function getAgeGroup(ageGroup) {
+    const ageGroupObj = _.find(subCampRows, x => x.ageGroup === ageGroup) || {};
+    return Number(ageGroupObj.count) || 0;
+  }
+
+  const child = getAgeGroup('Lapsi 0-2v') + getAgeGroup('Lapsi 3-7v');
+  const supe = getAgeGroup('Sudenpentu 7-9v');
+  const seikkailija = getAgeGroup('Seikkailija 10-12v');
+  const tarpoja = getAgeGroup('Tarpoja 12-15v');
+  const samoaja = getAgeGroup('Samoaja 15-17v');
+  const vaeltaja = getAgeGroup('Vaeltaja 18-22v');
+  const adult = getAgeGroup('Aikuinen');
+  const total = child + supe + seikkailija + tarpoja + samoaja + vaeltaja + adult;
+
+  return {
+    subCamp,
+    child,
+    supe,
+    seikkailija,
+    tarpoja,
+    samoaja,
+    vaeltaja,
+    adult,
+    total,
+  };
+}
+
+function calculateAgeGroupSum(counts) {
+  return counts.reduce((acc, elem) => ({
+    child: acc.child + elem.child,
+    supe: acc.supe + elem.supe,
+    seikkailija: acc.seikkailija + elem.seikkailija,
+    tarpoja: acc.tarpoja + elem.tarpoja,
+    samoaja: acc.samoaja + elem.samoaja,
+    vaeltaja: acc.vaeltaja + elem.vaeltaja,
+    adult: acc.adult + elem.adult,
+    total: acc.total + elem.total,
+  }), {
+    child: 0,
+    supe: 0,
+    seikkailija: 0,
+    tarpoja: 0,
+    samoaja: 0,
+    vaeltaja: 0,
+    adult: 0,
+    total: 0,
   });
 }
