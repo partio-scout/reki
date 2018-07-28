a5044b79632ba70458806cadca987f414db3334f
Miika Hänninen <miika.hanninen@reaktor.com>
2018-07-28 11:44:36 +0300

    Fixup presence history endpoint

--- src/server/boot/03-presence-history-endpoint.js
+++ src/server/boot/03-presence-history-endpoint.js
@@ -5,15 +5,15 @@ import { sequelize } from '../models';
 export default function(app) {
   app.get('/api/presence-history', async (req, res, next) => {
     try {
-      const result = await sequelize.query(`SELECT participants."participantId", presence_histories.timestamp,presence_histories.presence FROM participants LEFT JOIN "presence_histories" ON participants."participantId"  = presence_histories."participantParticipantId"`, { type: Sequelize.QueryTypes.SELECT });
-      const grouped = _.groupBy(result, row => row.participantId);
+      const result = await sequelize.query(`SELECT participants."memberNumber", presence_histories.timestamp, presence_histories.presence FROM participants LEFT JOIN "presence_histories" ON participants."participantId" = presence_histories."participantParticipantId"`, { type: Sequelize.QueryTypes.SELECT });
+      const grouped = _.groupBy(result, row => row.memberNumber);
 
       res.type('text/csv');
       res.writeHead(200);
-      for (const participantId of Object.keys(grouped)) {
-        const rows = grouped[participantId];
+      for (const memberNumber of Object.keys(grouped)) {
+        const rows = grouped[memberNumber];
         const presences = rows.map(row => row.timestamp ? `${row.timestamp.toISOString()} -> ${row.presence}` : '').join(' | ');
-        res.write(`${participantId},${presences}\n`);
+        res.write(`${memberNumber},${presences}\n`);
       }
       res.end();
     } catch (e) {
