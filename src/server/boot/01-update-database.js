ea889b55a305ba6c8b1e73cc0071d90af6b37ded
Miika Hänninen <miika.hanninen@reaktor.com>
2018-08-11 20:27:22 +0300

    Add a db connection check to bootup
    When using docker compose, postgres is started at the same time as reki,
    but postgres takes longer to boot than reki, leading to crashes. This
    eliminates those crashes.

--- src/server/boot/01-update-database.js
+++ src/server/boot/01-update-database.js
@@ -7,5 +7,36 @@ export default async function(app) {
     return;
   }
 
+  await waitForDbConnection();
+
   await sequelize.sync({ alter: true });
+}
+
+async function waitForDbConnection() {
+  let triesLeft = 5;
+  const delay = exponentialBackoffDelay(1000, 1.1);
+
+  while (true) { // eslint-disable-line no-constant-condition
+    try {
+      await sequelize.authenticate();
+      return;
+    } catch (e) {
+      triesLeft--;
+      if (triesLeft <= 0) {
+        throw new Error('Could not connect to the database');
+      }
+
+      await delay();
+    }
+  }
+}
+
+function exponentialBackoffDelay(baseDelayMs, backoffMultiplier) {
+  let currentDelay = baseDelayMs;
+  return function delay() {
+    return new Promise(resolve => {
+      setTimeout(resolve, currentDelay);
+      currentDelay *= backoffMultiplier;
+    });
+  };
 }
