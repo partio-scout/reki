c57afb37b6913ec1c057605761b43d80fb8d4659
Emil Virkki <emil.virkki@gmail.com>
2019-02-09 20:55:39 +0200

    Remove auth implementation details from participant date test

--- test/functional/participant-date.test.js
+++ test/functional/participant-date.test.js
@@ -1,12 +1,8 @@
-import app from '../../src/server/server';
-import request from 'supertest';
 import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
 import * as testUtils from '../utils/test-utils';
 import { resetDatabase } from '../../scripts/seed-database';
 
 const expect = chai.expect;
-chai.use(chaiAsPromised);
 
 const testParticipants = [
   {
@@ -48,47 +44,27 @@ const testParticipantDates = [
   { participantId: 2, date: new Date(2016,6,27) },
 ];
 
-const testUser = {
-  'id': 3,
-  'username': 'testLooser',
-  'memberNumber': '00000002',
-  'email': 'jukka.pekka@example.com',
-  'password': 'salasa',
-  'firstName': 'Jukka',
-  'lastName': 'Pekka',
-  'phoneNumber': '0000000003',
-};
-
-describe('particpantDates', () => {
-
-  let accessToken = null;
+describe('particpantDates endpoint', () => {
 
   beforeEach(async () => {
     await resetDatabase();
-    accessToken = await testUtils.createUserAndGetAccessToken(['registryUser'], testUser);
     await testUtils.createFixtureSequelize('Participant', testParticipants);
     await testUtils.createFixtureSequelize('ParticipantDate', testParticipantDates);
-    accessToken = accessToken.id;
   });
 
-  afterEach(async () => {
-    await testUtils.deleteFixturesIfExistSequelize('Participant');
-    await testUtils.deleteFixturesIfExistSequelize('ParticipantDate');
-    await testUtils.deleteFixturesIfExist('RegistryUser');
-  });
+  afterEach(resetDatabase);
 
-  it('GET request to participantdates', async () =>
-    await request(app)
-    .get(`/api/participantdates/?access_token=${accessToken}&filter=[fields][date]=true`)
-    .expect(200)
-    .expect(res => {
-      expect(res.body).to.be.an('array').with.length(5);
-      expect(res.body[0]).to.have.property('date','2016-07-20T00:00:00.000Z');
-      expect(res.body[1]).to.have.property('date','2016-07-21T00:00:00.000Z');
-      expect(res.body[2]).to.have.property('date','2016-07-22T00:00:00.000Z');
-      expect(res.body[3]).to.have.property('date','2016-07-23T00:00:00.000Z');
-      expect(res.body[4]).to.have.property('date','2016-07-27T00:00:00.000Z');
-    })
-  );
+  it('it returns unique dates when participants may be present', async () => {
+    const res = await testUtils.getWithRoles('/api/participantdates', ['registryUser']);
+
+    testUtils.expectStatus(res.status, 200);
+
+    expect(res.body).to.be.an('array').with.length(5);
+    expect(res.body[0]).to.have.property('date','2016-07-20T00:00:00.000Z');
+    expect(res.body[1]).to.have.property('date','2016-07-21T00:00:00.000Z');
+    expect(res.body[2]).to.have.property('date','2016-07-22T00:00:00.000Z');
+    expect(res.body[3]).to.have.property('date','2016-07-23T00:00:00.000Z');
+    expect(res.body[4]).to.have.property('date','2016-07-27T00:00:00.000Z');
+  });
 
 });
