962bd065f4e30827838a6043d71e164c4bbc89a9
Emil Virkki <emil.virkki@gmail.com>
2019-02-09 20:42:49 +0200

    Remove auth implementation details from options test

--- test/functional/options.test.js
+++ test/functional/options.test.js
@@ -1,23 +1,8 @@
-import app from '../../src/server/server';
-import request from 'supertest';
 import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
 import * as testUtils from '../utils/test-utils';
 import { resetDatabase } from '../../scripts/seed-database';
 
 const expect = chai.expect;
-chai.use(chaiAsPromised);
-
-const testUser = {
-  'id': 1,
-  'username': 'testLooser',
-  'memberNumber': '00000002',
-  'email': 'jukka.pekka@example.com',
-  'password': 'salasa',
-  'firstName': 'Jukka',
-  'lastName': 'Pekka',
-  'phoneNumber': '0000000003',
-};
 
 const testOptions = [
   {
@@ -36,33 +21,24 @@ const testOptions = [
 
 describe('Options', () => {
 
-  let accessToken = null;
-
-  beforeEach( async () => {
+  beforeEach(async () => {
     await resetDatabase();
-    accessToken = await testUtils.createUserAndGetAccessToken(['registryUser'], testUser);
     await testUtils.createFixtureSequelize('Option', testOptions);
-    accessToken = accessToken.id;
   });
 
-  afterEach( async () => {
-    await testUtils.deleteFixturesIfExistSequelize('Option');
-    await testUtils.deleteFixturesIfExist('RegistryUser');
-  });
+  afterEach(resetDatabase);
 
-  it('GET request to options', async () =>
-    await request(app)
-    .get(`/api/options/?access_token=${accessToken}`)
-    .expect(200)
-    .expect(res => {
-      expect(res.body).to.be.an('array').with.length(3);
-      expect(res.body[0]).to.have.property('property','village');
-      expect(res.body[0]).to.have.property('value','Mallikylä');
-      expect(res.body[1]).to.have.property('property','village');
-      expect(res.body[1]).to.have.property('value','muu');
-      expect(res.body[2]).to.have.property('property','campGroup');
-      expect(res.body[2]).to.have.property('value','muu');
-    })
-  );
+  it('It returns filter options', async () => {
+    const res = await testUtils.getWithRoles('/api/options', ['registryUser']);
+    testUtils.expectStatus(res.status, 200);
+
+    expect(res.body).to.be.an('array').with.length(3);
+    expect(res.body[0]).to.have.property('property','village');
+    expect(res.body[0]).to.have.property('value','Mallikylä');
+    expect(res.body[1]).to.have.property('property','village');
+    expect(res.body[1]).to.have.property('value','muu');
+    expect(res.body[2]).to.have.property('property','campGroup');
+    expect(res.body[2]).to.have.property('value','muu');
+  });
 
 });
