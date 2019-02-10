c5b001185c24566ebe8018977946b83e24093bbc
Emil Virkki <emil.virkki@gmail.com>
2019-02-10 21:25:25 +0200

    Clarify where test users are created in search filter test

--- test/functional/search-filter.test.js
+++ test/functional/search-filter.test.js
@@ -11,13 +11,20 @@ describe('SearchFilter', () => {
     filter: '?filter=%7B"textSearch"%3A"durpdurp"%7D',
   };
 
+  let user;
+
   before(resetDatabase);
 
-  afterEach(() => testUtils.deleteFixturesIfExistSequelize('SearchFilter'));
+  beforeEach(async () => user = await testUtils.createUserWithRoles(['registryUser']));
+
+  afterEach(async () => {
+    await testUtils.deleteFixturesIfExistSequelize('SearchFilter');
+    await testUtils.deleteFixturesIfExist('RegistryUser');
+  });
 
   it('GET SearchFilter returns a filter when not empty', async () => {
     await testUtils.createFixtureSequelize('SearchFilter', searchFilterFixture);
-    const res = await testUtils.getWithRoles('/api/searchfilters', ['registryUser']);
+    const res = await testUtils.getWithUser('/api/searchfilters', user);
     testUtils.expectStatus(res.status, 200);
 
     expect(res.body).to.be.an('array').with.length(1);
@@ -27,15 +34,15 @@ describe('SearchFilter', () => {
   });
 
   it('GET SearchFilter returns empty array when there are no search filters', async () => {
-    const res = await testUtils.getWithRoles('/api/searchfilters', ['registryUser']);
+    const res = await testUtils.getWithUser('/api/searchfilters', user);
     testUtils.expectStatus(res.status, 200);
     expect(res.body).to.be.an('array').with.length(0);
   });
 
   it('POST request to SearchFilter returns the filter and saves it to database', async () => {
-    const res = await testUtils.postWithRoles(
+    const res = await testUtils.postWithUser(
       '/api/searchfilters',
-      ['registryUser'],
+      user,
       {
         filter:'?filter=%7B%22textSearch%22%3A%22asd%22%7D',
         name:'ok',
@@ -53,7 +60,7 @@ describe('SearchFilter', () => {
 
   it('DELETE request destroys the filter we tried to destroy from the database', async () => {
     const filter = await models.SearchFilter.create(searchFilterFixture);
-    const res = await testUtils.deleteWithRoles(`/api/searchfilters/${filter.id}`, ['registryUser']);
+    const res = await testUtils.deleteWithUser(`/api/searchfilters/${filter.id}`, user);
     testUtils.expectStatus(res.status, 200);
 
     const filters = await models.SearchFilter.findAll();
@@ -62,7 +69,7 @@ describe('SearchFilter', () => {
 
   it('DELETE request returns 404 if filter is not found', async () => {
     const filter = await testUtils.createFixtureSequelize('SearchFilter', searchFilterFixture);
-    const res = await testUtils.deleteWithRoles(`/api/searchfilters/${filter.id+1}`, ['registryUser']);
+    const res = await testUtils.deleteWithUser(`/api/searchfilters/${filter.id+1}`, user);
     testUtils.expectStatus(res.status, 404);
 
     const filters = await models.SearchFilter.findAll();
@@ -70,7 +77,7 @@ describe('SearchFilter', () => {
   });
 
   it('DELETE request returns 404 for non-integer ids', async () => {
-    const res = await testUtils.deleteWithRoles('/api/searchfilters/hello', ['registryUser']);
+    const res = await testUtils.deleteWithUser('/api/searchfilters/hello', user);
     testUtils.expectStatus(res.status, 404);
   });
 
