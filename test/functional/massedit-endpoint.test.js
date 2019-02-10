ffb123a88f612ee40d016b69106bc69f9a9d2275
Emil Virkki <emil.virkki@gmail.com>
2019-02-10 21:02:41 +0200

    Make it clearer where test user are created in mass assign test

--- test/functional/massedit-endpoint.test.js
+++ test/functional/massedit-endpoint.test.js
@@ -1,12 +1,10 @@
 import { models } from '../../src/server/models';
 import _ from 'lodash';
 import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
 import * as testUtils from '../utils/test-utils';
 import { resetDatabase } from '../../scripts/seed-database';
 
 const expect = chai.expect;
-chai.use(chaiAsPromised);
 
 describe('Participant mass edit endpoint test', () => {
   const inCamp = 3;
@@ -61,15 +59,18 @@ describe('Participant mass edit endpoint test', () => {
     },
   ];
 
+  let user;
+
   beforeEach(async() => {
     await resetDatabase();
     await testUtils.createFixtureSequelize('Participant', testParticipants);
+    user = await testUtils.createUserWithRoles(['registryUser']);
   });
 
   after(resetDatabase);
 
   it('Should update whitelisted fields', async () => {
-    const res = await testUtils.postWithRoles('/api/participants/massAssign', ['registryUser'], {
+    const res = await testUtils.postWithUser('/api/participants/massAssign', user, {
       ids: [ 1,2 ],
       newValue: inCamp,
       fieldName: 'presence',
@@ -84,7 +85,7 @@ describe('Participant mass edit endpoint test', () => {
   });
 
   it('Should not update fields that are not whitelisted', async () => {
-    const res = await testUtils.postWithRoles('/api/participants/massAssign', ['registryUser'], {
+    const res = await testUtils.postWithUser('/api/participants/massAssign', user, {
       ids: [ 1,2 ],
       newValue: 'alaleiri2',
       fieldName: 'subCamp',
