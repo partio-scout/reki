21f72c3fc1440bf74dae1e7c01311bfcbfdeb622
Miika Hänninen <miika.hanninen@reaktor.com>
2019-09-07 18:23:54 +0300

    Remove deprecated sequelize option

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -5,7 +5,6 @@ import appModels from './app-models.js';
 const dbUrl = process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 const db = new Sequelize(dbUrl, {
   logging: false,
-  operatorsAliases: false,
 });
 
 export const sequelize = db;
