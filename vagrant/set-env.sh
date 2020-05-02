7ef9e5f03d05085e8bf14df27d386de33d94ac89
Miika Hänninen <miika.hanninen@partio.fi>
2020-05-02 12:57:15 +0300

    Remove vagrant directory

--- vagrant/set-env.sh
+++
@@ -1,4 +0,0 @@
-export KUKSA_API_ENDPOINT=http://localhost:4001
-export KUKSA_API_USERNAME=username
-export KUKSA_API_PASSWORD=password
-export KUKSA_API_EVENTID=c3ef7c48-1df4-4bf8-b1d0-d523d601a3e1
