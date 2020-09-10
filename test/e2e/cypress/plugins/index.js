6ec7a41c16df1b911d6d5dc683462dc978e839f9
Emil Virkki <emil.virkki@gmail.com>
2020-09-10 20:57:11 +0300

    Add test for login and ability to create users in Cypress

--- test/e2e/cypress/plugins/index.js
+++ test/e2e/cypress/plugins/index.js
@@ -12,10 +12,23 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+import {
+  createUserWithRoles,
+  deleteFixturesIfExist,
+} from '../../../utils/test-utils'
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('task', {
+    createUser(overrides) {
+      return createUserWithRoles(['registryUser', 'registryAdmin'], overrides)
+    },
+    deleteFixtures(model) {
+      return deleteFixturesIfExist(model)
+    },
+  })
 }
--- test/e2e/specs/smoke_spec.js
+++ test/e2e/specs/smoke_spec.js
@@ -1,6 +1,29 @@
 describe('REKI', () => {
-  it('displays a login link', () => {
+  beforeEach(() => {
+    cy.task('createUser', {
+      email: 'masa',
+      password: 'Salasana123',
+      firstName: 'Matti',
+      lastName: 'Mallikas',
+    })
+  })
+
+  it('displays a login link when not logged in', () => {
     cy.visit('/')
     cy.contains('Kirjaudu')
+  })
+
+  it('displays the current user when logged in', () => {
+    cy.visit('/login/password', {
+      auth: {
+        username: 'masa',
+        password: 'Salasana123',
+      },
+    })
+    cy.contains('Mallikas')
+  })
+
+  afterEach(() => {
+    cy.task('deleteFixtures', 'User')
   })
 })
